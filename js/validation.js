fc2d90a2e6908c0d84883d944bf1753d96abe5d0
Paliy Diana <dpaliy@oggettoweb.com>
2015-02-17 13:13:37 +0300

    validation2

--- js/validation.js
+++ js/validation.js
@@ -1,28 +1,12 @@
 $().ready(function() {
     $("#subscribeForm").validate({
-        rules: {
-            firstname: "required",
-            lastname: "required",
-            email: {
-                required: true,
-                email: true
-            },
-            city: "required",
-            street: "required",
-            zip: {
-                required: true,
-                number: true
-            },
-            phone: "required"
-        },
         messages: {
             firstname: "Enter firstname",
             lastname: "Enter lastname",
             city: "enter city",
             street: "enter street",
             zip: {
-                required: "Enter zip",
-                number: "Enter number"
+                required: "Enter zip"
             },
             phone: "enter phone number"
         }
