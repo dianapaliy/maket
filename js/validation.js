91846e60466cc4fa68622287109cc4a0bb0888cc
Paliy Diana <dpaliy@oggettoweb.com>
2015-02-17 14:48:59 +0300

    validation3

--- js/validation.js
+++ js/validation.js
@@ -6,11 +6,22 @@ $().ready(function() {
             city: "enter city",
             street: "enter street",
             zip: {
-                required: "Enter zip"
+                required: "Enter zip",
+                digits: "numbers!!!"
             },
             phone: "enter phone number"
+        },
+        errorPlacement: function(error, element) {
+            offset = element.offset();
+            error.css('display', 'none');   
         }
     });
-        $("#phone").mask("+7 (999) 999-99-99");
+    $("#phone").mask("+7 (999) 999-99-99");
+
+    $('#subscribeForm').submit(function() {
+
+        $('#subscribeForm').addClass('error-on-submit');
+
+    });
 }
 )
