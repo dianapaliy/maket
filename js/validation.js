a6187366fee1cfa6016117d6699dc4c89296e3fe
Diana Paliy <dpaliy@oggettoweb.com>
2015-03-16 18:52:20 +0300

    animation, validation, errors messages

--- js/validation.js
+++ js/validation.js
@@ -1,17 +1,5 @@
 $().ready(function() {
-    $("#subscribeForm").validate({
-        //messages: {
-        //    firstname: "Enter firstname",
-        //    lastname: "Enter lastname",
-        //    city: "enter city",
-        //    street: "enter street",
-        //    zip: {
-        //        required: "Enter zip",
-        //        digits: "numbers!!!"
-        //    },
-        //    phone: "enter phone number"
-        //}
-    });
+    $("#subscribeForm").validate();
     $("#phone").mask("+7 (999) 999-99-99");
 
     $('#subscribeForm').submit(function() {
@@ -19,5 +7,36 @@ $().ready(function() {
         $('#subscribeForm').addClass('error-on-submit').removeClass("subscribe-form");
 
     });
+
+    $('#zip').bind("change keyup input click", function() {
+        if (this.value.match(/[^0-9]/g)) {
+            this.value = this.value.replace(/[^0-9]/g, '');
+        }
+    });
+
+    $('.js-link').on('click', function(e){
+        e.preventDefault();
+
+        var $show = $(this).find('.js-show'),
+            $hide = $(this).find('.js-hide');
+
+        if($show.css('display') == 'inline-block'){
+            $show.hide();
+            $hide.css({'display': 'inline-block'});
+            $('.js-hide-block').show();
+
+            $('#age').bind("change keyup input click", function() {
+                if (this.value.match(/[^0-9]/g)) {
+                    this.value = this.value.replace(/[^0-9]/g, '');
+                }
+            });
+        }
+        else{
+                $hide.hide();
+                $show.css({'display': 'inline-block'});
+                $('.js-hide-block').hide();
+        }
+    });
+
 }
-)
+);
